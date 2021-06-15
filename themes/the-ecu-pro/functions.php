60cd4662cc52b9dfc9cb2c98678b7a69f6442e3d
marktnorman <marktnorman@gmail.com>
2021-06-15 21:31:56 +0200

    Updated FE

--- themes/the-ecu-pro/functions.php
+++ themes/the-ecu-pro/functions.php
@@ -224,7 +224,7 @@ function child_theme_admin_styles()
 add_action('wp_enqueue_scripts', 'theecupro_enqueue_assets');
 function theecupro_enqueue_assets()
 {
-    $version = '10.3.1';
+    $version = '10.3.2';
 
     // CARRY ON
     wp_enqueue_style('theecupro-default-style', get_stylesheet_uri());
