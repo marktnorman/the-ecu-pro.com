aa3cb6faa0e82d9711dc1a54675904908b78ebaf
marktnorman <marktnorman@gmail.com>
2021-06-24 00:01:22 +0200

    Updated yoast google json output

--- themes/the-ecu-pro/woocommerce/single-product/add-to-cart/variable.php
+++ themes/the-ecu-pro/woocommerce/single-product/add-to-cart/variable.php
@@ -84,9 +84,18 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
                     <?php else : ?>
                         <table class="variations" cellspacing="0">
                             <tbody>
-                            <?php foreach ($attributes as $attribute_name => $options) : ?>
+                            <?php foreach ($attributes as $attribute_name => $options) :
+
+                                $selected = isset(
+                                    $_REQUEST['attribute_' . sanitize_title(
+                                        $attribute_name
+                                    )]
+                                ) ? wc_clean(
+                                    $_REQUEST['attribute_' . sanitize_title($attribute_name)]
+                                ) : $product->get_variation_default_attribute($attribute_name);
+
+                                ?>
                                 <tr>
-                                    <span style="display: none !important;"><?php var_dump($options); ?></span>
                                     <td class="label"><label for="<?php echo esc_attr(
                                             sanitize_title($attribute_name)
                                         ); ?>"><?php echo wc_attribute_label(
