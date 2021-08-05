396b1f1d6f9631abce40a25c045ef6a0c73f9ee0
Mark Norman <mark@gsdh.org>
2021-08-05 20:47:32 +0200

    Updated FE

--- themes/the-ecu-pro/functions.php
+++ themes/the-ecu-pro/functions.php
@@ -224,7 +224,7 @@ function child_theme_admin_styles()
 add_action('wp_enqueue_scripts', 'theecupro_enqueue_assets');
 function theecupro_enqueue_assets()
 {
-    $version = '10.9.3';
+    $version = '10.9.4';
 
     // CARRY ON
     wp_enqueue_style('theecupro-default-style', get_stylesheet_uri());
@@ -1336,7 +1336,8 @@ function redirect_after_work_order_creation()
                 if (jQuery('#work-order-tag-container').length > 0) {
                     jQuery('.infoContainer').addClass('show');
                     jQuery('.infoContainer').removeClass('hide');
-                    jQuery('.work-order-tag-div').html(<?php echo $order_tag ?>);
+                    jQuery('#work-order-tag-container .wpcf7').addClass('hide');
+                    jQuery('.work-order-tag-div').text(<?php echo $order_tag; ?>);
                 }
             }
         }, false);
--- themes/the-ecu-pro/page_work_order_tag_template.php
+++ themes/the-ecu-pro/page_work_order_tag_template.php
@@ -15,15 +15,10 @@ get_header(); ?>
                         <div id="work-order-tag-container" class="woocommerce-order">
                             <div style="margin-top: 20px;">
 
-                                <div class="infoContainer hide"><em class="thankyouImage"><img
-                                                src="https://static.zohocdn.com/forms/images/green-tick.d80571f0156757619a5040ba1bda4f1a.gif"></em>
-
+                                <div class="infoContainer hide">
                                     <span id="richTxtMsgSpan" class="infoCont"> <label class="descFld">
 Thank you!<br><br><span class="colour" style="color: rgb(255, 51, 51)">Please write the following order number on your box with the parts that you send in:</span><br><br><span
-                                                    class="size" style="font-size: 18.6667px"><span class="work-order-tag-div"></span><br><br><a
-                                                        href="https://forms.zohopublic.com/theecupro/form/FRMRepairNeworder/publicrecord/z4DcfS5M8ALCfLp_QynD8-BUlBp0dT-0dxc_EquKig0"
-                                                        title="Edit information provided" target="_blank"
-                                                        rel="noopener noreferrer">Edit information provided</a><br></span><span
+                                                    class="size" style="font-size: 18.6667px"><span class="work-order-tag-div"></span><br><br><br></span><span
                                                     class="size" style="font-size: 48px"><br><br><br></span><div><br></div>
 </label> </span></div>
 
